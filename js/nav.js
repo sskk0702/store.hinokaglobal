e1d6f76ac347b48b0209df133896ca6e75de986e
sskk0702 <sh532650154@gmail.com>
2026-06-24 14:23:02 +0900

    Update nav.js

--- js/nav.js
+++ js/nav.js
@@ -99,7 +99,7 @@
       </a>
       <a class="right-drawer-item" href="b2b-dashboard.html" id="navB2BLink" style="display:none;">
         <svg viewBox="0 0 24 24"><path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"/><polyline points="9 22 9 12 15 12 15 22"/></svg>
-        法人ダッシュボード
+        マイアカウント（法人）
       </a>
       <a class="right-drawer-item" href="cart.html">
         <svg viewBox="0 0 24 24"><path d="M6 2L3 6v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V6l-3-4z"/><line x1="3" y1="6" x2="21" y2="6"/><path d="M16 10a4 4 0 0 1-8 0"/></svg>
